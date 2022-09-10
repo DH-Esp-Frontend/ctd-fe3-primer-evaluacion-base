f04f6870b92d064edd821702be2feb5f932d6113
Alejo <alejoivanrojas@gmail.com>
2022-09-10 15:51:22 -0300

    add validation tests

--- src/tests/Evaluacion.spec.jsx
+++ src/tests/Evaluacion.spec.jsx
@@ -9,29 +9,71 @@ describe("First exam Front III", ()=>{
         })
     })
     describe("Form", ()=>{
-        it("Should render at list two inputs", ()=>{
+        it("Should render at least two inputs", ()=>{
             render(<App/>)
             const inputs = screen.getAllByRole("textbox")
             expect(inputs.length).toBeGreaterThan(1)
         })
 
         describe('On error', () => {
-            it("Should show an error message", async ()=>{
-                render(<App/>)
-                const inputs = screen.getAllByRole("textbox")
-                const submitButton = screen.getByRole('button', {type: "submit"})
-                userEvent.type(inputs[0], "Just testing")
-                userEvent.type(inputs[1], "")
-                userEvent.click(submitButton)
+            describe("Should show an error message",()=>{
+                it("If the username length is less than 3", async ()=>{
+                    render(<App/>)
+                    const inputs = screen.getAllByRole("textbox")
+                    const submitButton = screen.getByRole('button', {type: "submit"})
+                    userEvent.type(inputs[0], "DH")
+                    userEvent.type(inputs[1], "Digital1234")
+                    userEvent.click(submitButton)
 
-                const errorMessage = await screen.findByText("Please check your information again")
-                const cardText1 = screen.queryByText("Just testing")
-                expect(errorMessage).toBeInTheDocument()
-                expect(cardText1).not.toBeInTheDocument() 
+                    const errorMessage = await screen.findByText("Please check your information again")
+                    const cardText1 = screen.queryByText("DH")
+                    expect(errorMessage).toBeInTheDocument()
+                    expect(cardText1).not.toBeInTheDocument() 
+                })
+                it("If the username contains blank spaces at the beginning", async ()=>{
+                    render(<App/>)
+                    const inputs = screen.getAllByRole("textbox")
+                    const submitButton = screen.getByRole('button', {type: "submit"})
+                    userEvent.type(inputs[0], "   DH")
+                    userEvent.type(inputs[1], "Digital1234")
+                    userEvent.click(submitButton)
+
+                    const errorMessage = await screen.findByText("Please check your information again")
+                    const cardText1 = screen.queryByText("   DH")
+                    expect(errorMessage).toBeInTheDocument()
+                    expect(cardText1).not.toBeInTheDocument() 
+                })
+                it("If the password length is less than 6", async ()=>{
+                    render(<App/>)
+                    const inputs = screen.getAllByRole("textbox")
+                    const submitButton = screen.getByRole('button', {type: "submit"})
+                    userEvent.type(inputs[0], "Just testing")
+                    userEvent.type(inputs[1], "code")
+                    userEvent.click(submitButton)
+
+                    const errorMessage = await screen.findByText("Please check your information again")
+                    const cardText1 = screen.queryByText("Just testing")
+                    expect(errorMessage).toBeInTheDocument()
+                    expect(cardText1).not.toBeInTheDocument() 
+                })
+                it("If the password does not contains any numbers", async ()=>{
+                    render(<App/>)
+                    const inputs = screen.getAllByRole("textbox")
+                    const submitButton = screen.getByRole('button', {type: "submit"})
+                    userEvent.type(inputs[0], "Just testing")
+                    userEvent.type(inputs[1], "code")
+                    userEvent.click(submitButton)
+
+                    const errorMessage = await screen.findByText("Please check your information again")
+                    const cardText1 = screen.queryByText("Just testing")
+                    expect(errorMessage).toBeInTheDocument()
+                    expect(cardText1).not.toBeInTheDocument() 
+                })
             })
-         })
+        })
+
         describe('On success', () => {
-            it("Should render the card with the proper information", async ()=>{
+            it("Should render a Card with the proper information", async ()=>{
                 render(<App/>)
                 const inputs = screen.getAllByRole("textbox")
                 const submitButton = screen.getByRole('button', {type: "submit"})
